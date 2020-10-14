b765db25b73f57b93369277c509cbcd0cbdc434d
jaytremmel <jaytremmel@gmail.com>
2020-10-14 12:13:52 -0400

    mabl-JS-time-date

---
+++ currenttime.js
@@ -0,0 +1,51 @@
+/**
+ * Run a small snippet of JavaScript during a mabl flow/journey
+ *
+ * @param {object} mablInputs - Object containing mabl inputs such as variables (mablInputs.variables).
+ *                              Use mablInputs.variables.user for user defined variables
+ *                              (For example myVar may be accessed as mablInputs.variables.user.myVar)
+ *
+ * @param {function} callback - A callback function that must be called to complete
+ *                              the javascript step and provide a value to the following
+ *                              steps of the flow/journey. A return statement from this
+ *                              function call will not provide any results for use
+ *                              in the following steps in this flow or journey.
+ */
+function mablJavaScriptStep(mablInputs, callback) {
+
+var date_format = '12'; /* FORMAT CAN BE 12 hour (12) OR 24 hour (24)*/
+
+
+var d       = new Date();
+var hour    = d.getHours();  /* Returns the hour (from 0-23) */
+var minutes     = d.getMinutes();  /* Returns the minutes (from 0-59) */
+var result  = hour;
+var ext     = '';
+
+if(date_format == '12'){
+    if(hour > 12){
+        ext = 'PM';
+        hour = (hour - 12);
+
+        if(hour < 10){
+            result = "0" + hour;
+        }else if(hour == 12){
+            hour = "00";
+            ext = 'AM';
+        }
+    }
+    else if(hour < 12){
+        result = ((hour < 10) ? "0" + hour : hour);
+        ext = 'AM';
+    }else if(hour == 12){
+        ext = 'PM';
+    }
+}
+
+if(minutes < 10){
+    minutes = "0" + minutes; 
+}
+
+result = result + ":" + minutes + ' ' + ext;
+callback(result)
+}
